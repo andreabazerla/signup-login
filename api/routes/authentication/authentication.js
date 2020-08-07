7db078bcf7dc0f17dc3564ee24da3228779167f5
Andrea Bazerla <andrea.bazerla@gmail.com>
2020-08-07 17:52:53 +0200

    Removed a row in authentication.js file in routes directory.

--- api/routes/authentication/authentication.js
+++ api/routes/authentication/authentication.js
@@ -6,6 +6,5 @@ const router = express.Router();
 
 router.post("/signup", userController.signup);
 router.post("/login", userController.login);
-router.post("/logout", userController.logout);
 
 module.exports = router;
