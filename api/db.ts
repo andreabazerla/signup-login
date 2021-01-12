347036ed0f4f4ac19565221e13c018313104360e
Andrea Bazerla <andrea.bazerla@gmail.com>
2021-01-12 02:21:46 +0100

    Modified db.ts file in api

--- api/db.ts
+++ api/db.ts
@@ -4,11 +4,11 @@ async function setDb(): Promise<any> {
   let dbURI;
 
   if (process.env.NODE_ENV === 'production') {
-    dbURI = process.env.MONGODB_URI;
+    dbURI = process.env.MONGODB_URI_PROD;
   } else if (process.env.NODE_ENV === 'test') {
     dbURI = process.env.MONGODB_URI_TEST;
   } else {
-    dbURI = process.env.MONGODB_URI_LOCAL;
+    dbURI = process.env.MONGODB_URI_DEV;
   }
 
   await mongoose.connect(dbURI, {
@@ -16,6 +16,10 @@ async function setDb(): Promise<any> {
     useFindAndModify: false,
     useCreateIndex: true,
     useUnifiedTopology: true,
+  });
+
+  mongoose.connection.on('connected', () => {
+    console.log('Database connected');
   });
 }
 
